45d2b9ab244a5a0db59bfa44682f83c382358b41
kashan <smkh.kashan@gmail.com>
2022-08-08 00:19:18 +0500

    template config

---
+++ template.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    // Placeholder used to rename and replace in files
+    // package.json, index.json, android/, ios/
+    placeholderName: "MyApp",
+
+    // Directory with template
+    templateDir: "./template",
+
+};
