ccae8ecee52de789237310a13e5689be2b78e227
SMKH-PRO <smkh.kashan@gmail.com>
2023-04-03 21:59:04 +0500

    fixes in movement of type fiolder

--- template/src/redux/actions/user.ts
+++ template/src/redux/actions/user.ts
@@ -1,6 +1,6 @@
 import { UserReducer } from '../../utils/enums/reducers.enums';
 
-import type { Action, UserState } from '../types/user.type';
+import type { Action, UserState } from '../../utils/types/redux/user.type';
 
 const setUser = (data: UserState['data']): Action => ({
   type: UserReducer.SET_USER,
