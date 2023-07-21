cfcc157a09fe20ca8ebf523d399eb5c8406abc57
KASHAN HAIDER <smkh.kashan@gmail.com>
2023-07-21 16:31:14 +0500

    changes

--- template/android/app/src/main/java/com/myapp/MainActivity.java
+++ template/android/app/src/main/java/com/myapp/MainActivity.java
@@ -5,6 +5,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
   
