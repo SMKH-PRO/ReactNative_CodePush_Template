b9d4f8a13299e82e57a53cd95c795cb050e7d69a
KASHAN HAIDER <smkh.kashan@gmail.com>
2023-07-27 17:03:58 +0500

    changes

---
+++ template/src/redux/actions/theme.ts
@@ -0,0 +1,18 @@
+import { ThemeReducer } from '../../utils/enums/reducers.enums';
+import {
+  SwitchDarkModeAction,
+  SetThemeAction,
+  ThemeState,
+} from '../../utils/types/redux/theme.type';
+
+const switchDarkMode = (bool: boolean): SwitchDarkModeAction => ({
+  type: ThemeReducer.DARK_MODE,
+  payload: { dark: bool },
+});
+
+const setTheme = (theme: ThemeState): SetThemeAction => ({
+  type: ThemeReducer.SET_THEME,
+  payload: theme,
+});
+
+export { switchDarkMode, setTheme };
